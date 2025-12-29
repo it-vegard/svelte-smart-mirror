90d0da616cbaf0c7dc2a7af28b2c0327a8b76e5e
Vegard Haugstvedt <it.vegard@gmail.com>
2025-12-29 18:57:16 +0100

    Add date to the clock module

--- src/lib/components/clock/Clock.svelte
+++ src/lib/components/clock/Clock.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
+	import { date } from 'valibot';
 
 	// Reactive variable to hold the current time
 	let currentTime = new Date();
@@ -23,16 +24,27 @@
 	$: hours = currentTime.getHours().toString().padStart(2, '0');
 	$: minutes = currentTime.getMinutes().toString().padStart(2, '0');
 	$: seconds = currentTime.getSeconds().toString().padStart(2, '0');
+	$: dateString = currentTime.toLocaleDateString('nb-NO', {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	});
 </script>
 
 <div class="clock">
 	{hours}:{minutes}:{seconds}
 </div>
+<div class="date">{dateString}</div>
 
 <style>
 	.clock {
 		font-family: monospace;
 		font-size: 5rem;
 		display: inline-block;
+	}
+	.date {
+		margin-top: 0.5rem;
+		text-transform: capitalize;
 	}
 </style>
