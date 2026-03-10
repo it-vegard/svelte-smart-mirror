d82de27a2c7f9c516f84cebc15b290b802ede6b2
Vegard Haugstvedt <it.vegard@gmail.com>
2026-03-10 11:29:29 +0100

    Enable static-site-generation for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,10 +11,18 @@ const config = {
 		experimental: {
 			remoteFunctions: true
 		},
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// adapter-static only supports static site generation, see https://svelte.dev/docs/kit/adapter-static for more information.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	},
 	compilerOptions: {
 		experimental: {
